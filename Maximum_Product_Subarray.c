e2bace8b6c069c4951820bd6513ac0243a23abb7
ZSShen <andy780328@hotmail.com>
2015-06-26 11:13:32 -0700

    WA code for Algorithm#152.

---
+++ Maximum_Product_Subarray.c
@@ -0,0 +1,47 @@
+
+int maxProduct(int* nums, int numsSize)
+{
+    if (numsSize == 0)
+        return 0;
+    if (numsSize == 1)
+        return nums[0];
+
+    int g_max = nums[0];
+    int l_max = 1;
+    int l_min = 1;
+
+    int idx = 0;
+    while (idx < numsSize) {
+        if (nums[idx] > 0) {
+            if (nums[idx] * l_max < l_max)
+                l_max = nums[idx];
+            else
+                l_max *= nums[idx];
+            l_min *= nums[idx];
+        } else if (nums[idx] < 0) {
+            if (nums[idx] * l_min > l_min) {
+                int temp = l_max;
+                l_max = l_min * nums[idx];
+                l_min = temp * nums[idx];
+            } else {
+                l_max *= nums[idx];
+                l_min *= nums[idx];
+            }
+        } else {
+            if (nums[idx] > g_max)
+                g_max = nums[idx];
+            if (idx < (numsSize - 1)) {
+                idx++;
+                l_max = nums[idx];
+                l_min = nums[idx];
+            }
+        }
+
+        if (l_max > g_max)
+            g_max = l_max;
+
+        idx++;
+    }
+
+    return g_max;
+}
