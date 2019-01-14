70d75e9856cab4986872e4911b40d746146ff5c2
ZSShen <andy.zsshen@gmail.com>
2019-01-14 22:25:18 +0800

    Add: Practice Algorithm#162.

--- LeetCode/162_Find_Peak_Element.cpp
+++ LeetCode/162_Find_Peak_Element.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums)
-    {
-        if (nums.size() == 0)
-            return -1;
-        if (nums.size() == 1)
-            return 0;
+    int findPeakElement(vector<int>& nums) {
 
-        int count = nums.size();
-        int prev = nums.at(0);
-        for (int i = 1 ; i < count ; ++i) {
-            int curr = nums.at(i);
-            if (curr < prev)
-                return i - 1;
-            prev = curr;
+        int size = nums.size();
+        if (size == 1) { // Please be careful about the edge case [-2147483648]
+            return 0;
         }
-        return count - 1;
+
+        int left = 0, right = size - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (mid == size - 1) {
+                if (nums[mid] > nums[mid - 1]) {
+                    return mid;
+                }
+                right = mid - 1;
+                continue;
+            }
+
+            if (mid == 0) {
+                if (nums[mid] > nums[mid + 1]) {
+                    return mid;
+                }
+                left = mid + 1;
+                continue;
+            }
+
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+
+            if (nums[mid - 1] > nums[mid]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
     }
 };
