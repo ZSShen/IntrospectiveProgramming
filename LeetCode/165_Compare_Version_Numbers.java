41e6e68e0867458ff12a34adb17bbf3418e7ac47
ZSShen <andy.zsshen@gmail.com>
2019-01-17 09:22:20 +0800

    Modify: Review Algorithm#165 with Java.

--- LeetCode/165_Compare_Version_Numbers.java
+++ LeetCode/165_Compare_Version_Numbers.java
@@ -1,42 +1,17 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
 
-        String[] split1 = version1.split("\\.");
-        List<Integer> nums1 = new ArrayList<Integer>();
-        for (String str : split1) {
-            nums1.add(Integer.valueOf(str));
-        }
+        String[] nums_1 = version1.split("\\.");
+        String[] nums_2 = version2.split("\\.");
 
-        String[] split2 = version2.split("\\.");
-        List<Integer> nums2 = new ArrayList<Integer>();
-        for (String str : split2) {
-            nums2.add(Integer.valueOf(str));
-        }
+        int max = Math.max(nums_1.length, nums_2.length);
+        for (int i = 0 ; i < max ; ++i) {
+            Integer int_1 = i < nums_1.length ? Integer.parseInt(nums_1[i]) : 0;
+            Integer int_2 = i < nums_2.length ? Integer.parseInt(nums_2[i]) : 0;
 
-        int len1 = nums1.size();
-        int len2 = nums2.size();
-        int common = (len1 > len2)? len2 : len1;
-
-        for (int i = 0 ; i < common ; ++i) {
-            if (nums1.get(i) > nums2.get(i)) {
-                return 1;
-            } else if (nums1.get(i) < nums2.get(i)) {
-                return -1;
-            }
-        }
-
-        if (len1 > len2) {
-            for (int i = len2 ; i < len1 ; ++i) {
-                if (nums1.get(i) != 0 ) {
-                    return 1;
-                }
-            }
-        }
-        if (len2 > len1) {
-            for (int i = len1 ; i < len2 ; ++i) {
-                if (nums2.get(i) != 0) {
-                    return -1;
-                }
+            int order = int_1.compareTo(int_2);
+            if (order != 0) {
+                return order;
             }
         }
 
