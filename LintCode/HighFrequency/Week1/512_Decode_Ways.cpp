38271b3c6645687a3b074d7fe38ae5dc9411a4bf
ZSShen <andy.zsshen@gmail.com>
2019-06-26 15:27:02 +0800

    Add: Practice LintCode Algorithm #535, #667, #916, #1779.

--- LintCode/HighFrequency/Week1/512_Decode_Ways.cpp
+++ LintCode/HighFrequency/Week1/512_Decode_Ways.cpp
@@ -8,35 +8,62 @@ public:
         // write your code here
 
         /**
-         *   123
-         *   AW  => f(n) = f(n - 1) + f(n - 2)
-         *   LC
+         * dp[i]: The number of ways to decode the prefix ending at index i.
          *
-         *   10 -> 26
-         **/
+         * dp[i] = dp[i - 1] + dp[i - 2]
+         *
+         * - Handle 1 digit:
+         *   s[i]: 0              -> 0
+         *   s[i]: {1, 2, ..., 9} -> 1
+         *
+         * - Handle 2 digits:
+         *   s[i - 1]: 0                  -> 0
+         *   s[i - 1]: 1
+         *      - s[i]: {0, 1, 2, ..., 9} -> 1
+         *   s[i - 1]: 2
+         *      - s[i]: {0, 1, 2, ..., 6} -> 1
+         *      - s[i]: {7, 8, 9}         -> 0
+         *   s[i - 1]: {3, 4, ..., 9}     -> 0
+         */
 
-        int size = s.length();
-        if (size == 0) {
+        int n = s.length();
+        if (n == 0) {
             return 0;
         }
 
-        std::vector<int> dp(size + 1);
-        // For dummy initialization.
-        dp[0] = 1;
-        // For the first digit.
-        dp[1] = (s[0] != '0') ? 1 : 0;
+        std::vector<int> dp(n + 1, 1);
 
-        for (int i = 2 ; i <= size ; ++i) {
+        for (int i = 1 ; i <= n ; ++i) {
 
-            dp[i] = (s[i - 1] != '0') ? dp[i - 1] : 0;
-
-            if (s[i - 2] == '1' && ('0' <= s[i - 1] && s[i - 1] <= '9')) {
-                dp[i] += dp[i - 2];
-            } else if (s[i - 2] == '2' && ('0' <= s[i - 1] && s[i - 1] <= '6')) {
-                dp[i] += dp[i - 2];
+            dp[i] = countOneDigit(s, i - 1) * dp[i - 1];
+            if (i >= 2) {
+                dp[i] += countTwoDigits(s, i - 2, i - 1) * dp[i - 2];
             }
         }
 
-        return dp[size];
+        return dp[n];
+    }
+
+private:
+    int countOneDigit(const std::string& str, int s1) {
+
+        return (str[s1] == '0') ? 0 : 1;
+    }
+
+    int countTwoDigits(const std::string& str, int s1, int s2) {
+
+        if (str[s1] == '0') {
+            return 0;
+        }
+
+        if (str[s1] == '1') {
+            return 1;
+        }
+
+        if (str[s1] == '2') {
+            return ('0' <= str[s2] && str[s2] <= '6') ? 1 : 0;
+        }
+
+        return 0;
     }
 };
